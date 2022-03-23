9cae4071a0b75d3affdf2b9686904b7d48e39e23
Aniruddha09 <aniruddhashinde0807@gmail.com>
2022-03-23 18:34:31 +0530

    Added one more function

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,7 @@
-def call(String name, String city){
+def firstcall(String name, String city){
 	sh "echo Hello, My name is ${name} from ${city}"
+}
+def secondCall(String profile){
+	sh "echo He works in ${profile}"
+
 }
